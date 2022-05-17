becaf71918fb93789e8def56f624f64ef76ebbac
CESAR TZOC <csartalva01@gmail.com>
2022-05-17 03:12:29 -0600

    data for purchase on plane

--- trigger.sql
+++ trigger.sql
@@ -18,3 +18,37 @@ CREATE OR REPLACE TRIGGER set_price_ticket BEFORE INSERT ON airport."DETAIL_TICK
 FOR EACH ROW
 EXECUTE PROCEDURE setPriceOfTicket();
 -- obtener valor del boleto e ingresarlo
+
+-- trigger para calcular horas de vuelo y numero de vuelos
+CREATE OR REPLACE FUNCTION setFlightNumber()
+RETURN TRIGGER
+AS $set_flight_number$
+DECLARE
+	_years_of_work INT;
+BEGIN
+	SELECT years_of_work FROM airport."STAFF" WHERE person_id = NEW.person_id INTO _years_of_work;
+	NEW.flight_hours = 900 * _years_of_work;
+	NEW.number_of_flights = 144 * _years_of_work;
+	RETURN NEW;
+END;
+$set_flight_number$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER set_number_and_hours_of_flights BEFORE INSERT ON airport."ON_BOARD_STAFF"
+FOR EACH ROW
+EXECUTE PROCEDURE setFlightNumber();
+-- trigger para calcular horas de vuelo y numero de vuelos
+
+-- trigger para calcular valor total de la compra en el avion
+CREATE OR REPLACE FUNCTION setPriceOfPurchaseFlight()
+RETURNS TRIGGER
+AS $set_price_purchase_flight$
+BEGIN
+	UPDATE airport."PURCHASE_FLIGHT" SET purchase_total = purchase_total + NEW.purchase_subtotal WHERE purchase_flight_id = NEW.purchase_flight_id;
+	RETURN NEW;
+END;
+$set_price_purchase_flight$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER set_price_purchase_flight BEFORE INSERT ON airport."PURCHASE_FLIGHT_DETAIL"
+FOR EACH ROW
+EXECUTE PROCEDURE setPriceOfPurchaseFlight();
+-- trigger para calcular valor total de la compra en el avion
